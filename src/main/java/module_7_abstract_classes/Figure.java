4c36ea1d4f979ab9882b8e5dc2b8619077ed37c7
sanzharova <valvalsan@gmail.com>
2023-06-05 03:27:05 +0700

    Solution for exercise 7.6 added class Triangle

---
+++ src/main/java/module_7_abstract_classes/Figure.java
@@ -0,0 +1,12 @@
+package module_7_abstract_classes;
+
+abstract class Figure{
+
+    public abstract double area();
+
+    public abstract String pointsToString();
+
+    public String toString() {
+        return this.getClass().getSimpleName() + "[" + pointsToString() + "]";
+    }
+}
---
+++ src/main/java/module_7_abstract_classes/Triangle.java
@@ -0,0 +1,24 @@
+package module_7_abstract_classes;
+
+import static java.lang.Math.abs;
+
+public class Triangle extends Figure{
+
+    private Point apex1, apex2, apex3;
+
+    public Triangle(Point apex1, Point apex2, Point apex3){
+        this.apex1 = apex1;
+        this.apex2 = apex2;
+        this.apex3 = apex3;
+    }
+
+    public double area(){
+       return 0.5 * abs((apex2.getX() - apex1.getX()) * (apex3.getY() - apex1.getY())
+               - (apex3.getX() - apex1.getX()) * (apex2.getY() - apex1.getY()));
+    }
+
+    public String pointsToString(){
+        return apex1.toString() + apex2.toString() + apex3.toString();
+    }
+
+}
